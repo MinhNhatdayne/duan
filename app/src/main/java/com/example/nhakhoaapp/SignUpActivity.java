405ae0f2e850c87d93a256c943d8e5bc74c909f2
Hienmine <lehienmine@gmail.com>
2025-11-13 11:13:17 +0700

    đã chỉnh

---
+++ app/src/main/java/com/example/nhakhoaapp/SignUpActivity.java
@@ -0,0 +1,36 @@
+package com.example.nhakhoaapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText edtName, edtEmail, edtPassword, edtRePassword;
+    Button btnCreate;
+    TextView txtLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtRePassword = findViewById(R.id.edtRePassword);
+
+        btnCreate = findViewById(R.id.btnCreate);
+        txtLogin = findViewById(R.id.txtLogin);
+    }
+}
