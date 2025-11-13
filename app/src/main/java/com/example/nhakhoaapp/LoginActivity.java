ccd5c68ef7054c3ae8aa37106380000dd52defe2
Hienmine <lehienmine@gmail.com>
2025-11-13 11:01:25 +0700

    đã thêm layout sign up với login chưa thêm chức năng

--- app/src/main/java/com/example/nhakhoaapp/LoginActivity.java
+++ app/src/main/java/com/example/nhakhoaapp/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.example.nhakhoaapp;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText edtEmail, edtPassword;
-    private Button btnSignIn;
-    private TextView txtForgot, txtCreate;
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        edtEmail = findViewById(R.id.edtEmail);
-        edtPassword = findViewById(R.id.edtPassword);
-        btnSignIn = findViewById(R.id.btnSignIn);
-        txtForgot = findViewById(R.id.txtForgot);
-        txtCreate = findViewById(R.id.txtCreate);
     }
 }
